5d9bc5f177fe97d63197d0d27f00a52d8e3bd7ed
ipiknipsi <danielrtml@gmail.com>
2021-06-21 01:21:32 -0400

    Se ha implementado el CRUD modificar y solucion de detalles estéticos

---
+++ Entrega3 Crud Modificar v5.0/Entrega3/core/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path
+from .views import Index, galeria, contactenos, api, mod_feedback, registro, whorwe, inise, news, feedback, mod_feedback
+urlpatterns = [
+
+    path('', Index, name="Index"),
+    path('contactenos', contactenos, name="contactenos"),
+    path('galeria', galeria, name="galeria"),
+    path('api', api, name="api"),
+    path('registro', registro, name="registro"),
+    path('who-r-we', whorwe, name="who-r-we"),
+    path('ini-se', inise, name="ini-se"),
+    path('news', news, name="news"),
+    path('feedback', feedback, name="feedback"),
+    path('mod_feedback/<id>', mod_feedback, name="mod_feedback")
+
+    #metodos
+]
